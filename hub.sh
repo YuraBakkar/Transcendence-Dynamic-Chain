330252db5029f4083af1fa2ee03a8907ceeb9260
YuraBakkar <admin@pik.co.ua>
2018-09-27 21:46:11 +0300

    Update hub.sh

--- hub.sh
+++ hub.sh
@@ -179,6 +179,7 @@ wget https://raw.githubusercontent.com/YuraBakkar/arcticCore/master/ArcticCore.z
 wget https://raw.githubusercontent.com/YuraBakkar/arcticCore/master/ArcticCore.z02
 wget https://raw.githubusercontent.com/YuraBakkar/arcticCore/master/ArcticCore.z03
 wget https://raw.githubusercontent.com/YuraBakkar/arcticCore/master/ArcticCore.z04
+zip -s- ./ArcticCore.zip -O ac.zip
 fi
 IP4COUNT=$(find /root/.${COIN}_* -maxdepth 0 -type d | wc -l)
 
@@ -209,7 +210,7 @@ while [  $COUNTER -lt $MNCOUNT ]; do
 	read PORTD
   fi
   mkdir ~/.${COIN}_$ALIAS
-  unzip ArcticCore.zip -d ~/.${COIN}_$ALIAS >/dev/null 2>&1
+  unzip ac.zip -d ~/.${COIN}_$ALIAS >/dev/null 2>&1
   echo '#!/bin/bash' > ~/bin/${COIND}_$ALIAS.sh
   echo "${COIND} -daemon -conf=$CONF_DIR/${COIN}.conf -datadir=$CONF_DIR "'$*' >> ~/bin/${COIND}_$ALIAS.sh
   echo '#!/bin/bash' > ~/bin/${COINCLI}_$ALIAS.sh
